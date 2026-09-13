9a45249f04dda9b98a6140eb9762abb5175cffa4
Jatin Satare <jatinsatare@gmail.com>
2026-09-13 17:08:36 +0530

    return form with handleSearch action in SearchForm component

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -18,5 +18,20 @@ export default function SearchForm({search, startTransition}:{
     })
     
   }
- 
+
+  return (
+    <Form 
+      action={handleSearch}
+      className="w-full px-5 md:px-0 md:max-w-xl">
+      <input
+        type="text"
+        defaultValue={search}
+        id="search"
+        name="search"
+        placeholder="E.g. dragon"
+        autoComplete="off"
+        className="w-full py-3 pl-5 pr-5 text-sm placeholder-gray-500 bg-white border border-[#606060] rounded-full focus:border-[#606060] focus:outline-none focus:ring-0 md:text-base"
+      />
+    </Form>
+  )
 }
