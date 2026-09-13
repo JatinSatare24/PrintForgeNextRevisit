fb6c503598c70333ccd4838bdee2860e818b65d8
Jatin Satare <jatinsatare@gmail.com>
2026-09-13 17:10:41 +0530

    initialise sort controls component for sort functionality

---
+++ components/SortControls.tsx
@@ -0,0 +1,8 @@
+import SortButton from '@/components/SortButton'
+import type {TransitionStartFunction} from 'react'
+
+export default function SortControls({startTransition}:{
+  startTransition:TransitionStartFunction
+}) {
+ 
+}
