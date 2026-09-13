0bccd7363718b1f2a3319c5f14b60ad2aeada18a
Jatin Satare <jatinsatare@gmail.com>
2026-09-13 17:06:10 +0530

    initialise SearchForm component

---
+++ components/SearchForm.tsx
@@ -0,0 +1,11 @@
+import Form from 'next/form'
+import type {TransitionStartFunction} from 'react'
+import {useRouter, usePathname} from 'next/navigation'
+
+export default function SearchForm({search, startTransition}:{
+  search?:string,
+  startTransition:TransitionStartFunction
+}){
+
+ 
+}
