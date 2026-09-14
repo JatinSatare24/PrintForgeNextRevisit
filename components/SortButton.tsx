95fce2e7442c66ee0aa127987ab03a429c9e9baf
Jatin Satare <jatinsatare@gmail.com>
2026-09-14 12:25:58 +0530

    initialise SortButton component

---
+++ components/SortButton.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import {usePathname, useRouter, useSearchParams} from 'next/navigation'
+import type {TransitionStartFunction} from 'react'
+
+export default function SortButton({children, sort, startTransition}:{
+  children:React.ReactNode,
+  sort:string,
+  startTransition:TransitionStartFunction
+}){
+
+ 
+}
