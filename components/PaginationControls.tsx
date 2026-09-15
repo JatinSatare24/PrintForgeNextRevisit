0ba681c5783ed3288b9390f1b8fec2a8ddb66f46
Jatin Satare <jatinsatare@gmail.com>
2026-09-15 16:47:24 +0530

    add paginationButton components for rendering pagination buttons

--- components/PaginationControls.tsx
+++ components/PaginationControls.tsx
@@ -6,5 +6,21 @@ export default function PaginationControls({totalPages, currentPage}:{
 }){
   //[1, 2, 3, 4, 5]
   const pagesArray = Array.from({length:totalPages}, (_,i) => i + 1)
-  
+  return (
+    <div className="flex justify-center gap-1">  
+      {currentPage !== 1 && (
+        <PaginationButton page={1} label="<<"/>
+      )}
+      {currentPage !== 1 && (
+        <PaginationButton page={currentPage-1}/>
+      )}
+      <PaginationButton page={currentPage} isActive={true}/>
+      {currentPage !== totalPages && (
+        <PaginationButton page={currentPage+1}/>
+      )}
+      {currentPage !== totalPages && (
+        <PaginationButton page={totalPages} label=">>"/>
+      )}
+    </div>
+  )
 }
