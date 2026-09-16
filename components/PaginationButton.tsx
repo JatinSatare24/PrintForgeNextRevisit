5795e04fb0657e96333a9812eec1b42049725af6
Jatin Satare <jatinsatare@gmail.com>
2026-09-16 23:55:57 +0530

    initialise paginationButton component to render in paginationControls

---
+++ components/PaginationButton.tsx
@@ -0,0 +1,11 @@
+'use client'
+import {usePathname, useRouter, useSearchParams} from 'next/navigation'
+
+export default function PaginationButton({page, isActive, label}:{
+  page:number,
+  isActive:boolean,
+  label?:string
+}){
+
+ 
+}
