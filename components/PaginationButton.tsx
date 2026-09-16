29655b1ce650090d219598f2abb61253a106b6b7
Jatin Satare <jatinsatare@gmail.com>
2026-09-16 23:58:33 +0530

    render button with onclick handlePageChange in paginationButton component

--- components/PaginationButton.tsx
+++ components/PaginationButton.tsx
@@ -16,8 +16,13 @@ export default function PaginationButton({page, isActive, label}:{
     urlSearchParams.set('page', page.toString())
     const url = `${pathname}?${urlSearchParams.toString()}`
     router.push(url)
-    
   }
 
- 
+  return (
+    <button 
+      onClick={handlePageChange}
+      className={`px-3 py-1.5 text-sm rounded-md border cursor-pointer ${isActive ? 'text-white bg-orange-400 border-orange-300' : "border-gray-300 text-gray-700 hover:bg-gray-100"}`}>  
+      {label || page}  
+    </button>
+  )
 }
